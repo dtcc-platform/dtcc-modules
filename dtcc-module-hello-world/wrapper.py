706855e4ebb742e4a021ca3f4afe3757312d8523
Dag Wästberg <dwastberg@gmail.com>
2022-11-21 16:50:00 +0200

    bugfix

--- dtcc-module-hello-world/wrapper.py
+++ dtcc-module-hello-world/wrapper.py
@@ -7,6 +7,7 @@ import os
 from pubsub_client.run_in_shell import RunInShell
 from dtcc_hello_world import hello_world
 
+
 class DtccHelloWorld(RunInShell):
     def __init__(self, publish=True) -> None:
         RunInShell.__init__(self,
@@ -15,14 +16,15 @@ class DtccHelloWorld(RunInShell):
             shell_command="python3 dtcc_hello_world.py"
         )
         self.output_file = tempfile.NamedTemporaryFile(mode='w', delete=False)
+
     def process_arguments_on_start(self, message:dict):
         lang = message['lang']
         sleep_time = message['sleep_time']
         return f"{self.shell_command} --lang={lang} --sleep_time={sleep_time} --output-file={self.output_file.name}"
 
     def process_return_data(self):
-        with open(self.output_file.name, 'r') as f:
-            return_data = r.read()
+        with open(self.output_file.name, 'r') as src:
+            return_data = src.read()
         os.remove(self.output_file.name)
         return return_data
 
