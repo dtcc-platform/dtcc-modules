eeee75679aa76e8e5ea62bd5fea0befa8a04affb
Dag Wästberg <dwastberg@gmail.com>
2022-11-21 15:28:29 +0200

    call as cli

--- dtcc-module-hello-world/wrapper.py
+++ dtcc-module-hello-world/wrapper.py
@@ -1,4 +1,7 @@
+import subprocess
+
 from dtcc_hello_world import hello_world
+
 
 def start(command, data = {}, config = {}):
     if command == "hello-world":
@@ -6,5 +9,11 @@ def start(command, data = {}, config = {}):
         sleep_time = config['sleep_time']
         hi = hello_world(lang, sleep_time)
         return {"hello_world": hi}
+    if command == "hello-world-cli":
+        lang = config['lang']
+        sleep_time = config['sleep_time']
+        hi = subprocess.check_output(["dtcc-hello-world", "--lang", lang, "--sleep_time", str(sleep_time)])
+        return {"hello_world": hi}
+
     else:
         return {"error": "Command not found"}
