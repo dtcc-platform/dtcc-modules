bf92a31cb3b30d9c6698926a2fa762b81db25ab6
Anders Logg <logg@chalmers.se>
2022-11-22 11:56:42 +0200

    More work on DTCC Builder module

--- dtcc-module-dtcc-builder/wrapper.py
+++ dtcc-module-dtcc-builder/wrapper.py
@@ -11,31 +11,19 @@ from dtcc_hello_world import hello_world
 
 class DtccBuilder(RunInShell):
     def __init__(self, publish=True) -> None:
-        RunInShell.__init__(self,
-            task_name="dtcc-module-dtcc-builder",
-            publish=publish,
-            shell_command=""
-        )
-        self.message = {}
-        self.output_file = tempfile.NamedTemporaryFile(mode='w', delete=False)
+        RunInShell.__init__(self, 'dtcc-module-dtcc-builder', publish)
 
     def process_arguments_on_start(self, message:dict):
         self.message = message
-        command = message['command_name']
-        lang = message['lang']
-        sleep_time = message['sleep_time']
-        if command == "hello-world":
-            return f"python3 dtcc_hello_world.py --lang={lang} --sleep_time={sleep_time} --output-file={self.output_file.name}"
-        elif command == "hello-world-2":
-            return f"python3 dtcc_hello_world.py --lang={lang} --sleep_time={sleep_time} --output-file={self.output_file.name}"
+        if message['command_name'] == 'build-citymodel':
+            # FIXME: Need path to data directory (and the actual data...)
+            return f'dtcc-generate-citymodel <path_to_data_directory>'
 
     def process_return_data(self):
-        with open(self.output_file.name, 'r') as src:
-            return_data = src.read()
-        if self.message['command_name'] == "hello-world-2":
-            return_data += "from hello-world-2"
-        os.remove(self.output_file.name)
-        return json.dumps({"hello_world": return_data})
+        if self.message['command_name'] == 'build-citymodel':
+            # FIXME: Need to store the data on the file server
+            data = 'CityModel.pb'
+        return json.dumps({"data": data})
 
 if __name__ == "__main__":
     DtccBuilder(publish=True).listen()
