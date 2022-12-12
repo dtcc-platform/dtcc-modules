9cd0900f0c1f0afb835fb1c08ec5e202669f7687
Dag Wästberg <dwastberg@gmail.com>
2022-12-12 17:42:39 +0100

    WIP fixing dtcc/builder

---
+++ dtcc-module-dtcc-builder/_tools.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+import subprocess
+import tempfile
+import json
+import os, sys, pathlib
+
+
+# project_dir = str(pathlib.Path(__file__).resolve().parents[1])
+# sys.path.append(project_dir)
+
+from pubsub_client.run_in_shell import RunInShell
+
+
+class DtccBuilder(RunInShell):
+    def __init__(self, publish=False) -> None:
+        RunInShell.__init__(self, 
+            module='dtcc-module-dtcc-builder',
+            tool="build-citymodel", 
+            publish=publish,
+            shell_command="ls"
+        )
+
+    # Suggested new interface
+
+    def run_command(self, message:dict):
+        pass
+
+    def process_input(self):
+        self.data_directory = "/"
+        # Read point cloud from .las
+        # Write point cloud to .pb
+
+    def process_output(self):
+        pass
+
+    # Old interface
+
+    def process_arguments_on_start(self, message:dict):
+        self.message = message
+        if message['tool'] == 'build-citymodel':
+            return f'dtcc-generate-citymodel {self.data_directory}'
+        elif message['tool'] == 'build-mesh':
+            return f'dtcc-generate-mesh {self.data_directory}'
+
+    def process_return_data(self):
+        # if self.message['command_name'] == 'build-citymodel':
+        #     data = 'CityModel.pb'
+        # elif self.message['command_name'] == 'build-mesh':
+        #     data = 'CitySurface.pb'
+        return json.dumps({"data": ""})
+
+
+class DtccBuilderCityModel(RunInShell):
+    def __init__(self, publish=False) -> None:
+        RunInShell.__init__(self, 
+            module='dtcc-module-dtcc-builder',
+            tool="build-citymodel", 
+            publish=publish,
+            shell_command=""
+        )
+
+    def process_arguments_on_start(self, message:dict):
+        self.message = message
+        return f'dtcc-generate-citymodel {self.data_directory}'
+      
+    def process_return_data(self):
+        data = 'CityModel.pb'
+        return json.dumps({"data": data})
+
+if __name__ == "__main__":
+    DtccBuilderCityModel(publish=True).listen()
--- dtcc-module-dtcc-builder/tools.py
+++ dtcc-module-dtcc-builder/tools.py
@@ -2,71 +2,118 @@
 
 import subprocess
 import tempfile
-import json
 import os, sys, pathlib
-
+import json
 
 # project_dir = str(pathlib.Path(__file__).resolve().parents[1])
 # sys.path.append(project_dir)
 
 from pubsub_client.run_in_shell import RunInShell
+from pubsub_client.logger import getLogger
+logger = getLogger("dtcc-module-dtcc-builder")
 
-
-class DtccBuilder(RunInShell):
-    def __init__(self, publish=False) -> None:
-        RunInShell.__init__(self, 
-            module='dtcc-module-dtcc-builder',
-            tool="build-citymodel", 
-            publish=publish,
-            shell_command="ls"
-        )
-
-    # Suggested new interface
-
-    def run_command(self, message:dict):
-        pass
-
-    def process_input(self):
-        self.data_directory = "/"
-        # Read point cloud from .las
-        # Write point cloud to .pb
-
-    def process_output(self):
-        pass
-
-    # Old interface
-
-    def process_arguments_on_start(self, message:dict):
-        self.message = message
-        if message['tool'] == 'build-citymodel':
-            return f'dtcc-generate-citymodel {self.data_directory}'
-        elif message['tool'] == 'build-mesh':
-            return f'dtcc-generate-mesh {self.data_directory}'
-
-    def process_return_data(self):
-        # if self.message['command_name'] == 'build-citymodel':
-        #     data = 'CityModel.pb'
-        # elif self.message['command_name'] == 'build-mesh':
-        #     data = 'CitySurface.pb'
-        return json.dumps({"data": ""})
-
-
-class DtccBuilderCityModel(RunInShell):
-    def __init__(self, publish=False) -> None:
-        RunInShell.__init__(self, 
-            module='dtcc-module-dtcc-builder',
-            tool="build-citymodel", 
+class DtccCalcBuildingHeights(RunInShell):
+    def __init__(self, publish=True) -> None:
+        RunInShell.__init__(self,
+            module="dtcc-module-dtcc-builder",
+            tool="set-building-heights",
             publish=publish,
             shell_command=""
         )
 
-    def process_arguments_on_start(self, message:dict):
-        self.message = message
-        return f'dtcc-generate-citymodel {self.data_directory}'
-      
-    def process_return_data(self):
-        data = 'CityModel.pb'
-        return json.dumps({"data": data})
+
+    def run_command(self, parameters:dict) -> str:
+        """
+        Pass in arguments based on the recived parameters if needed
+        Otherwise just return the original shell command
+        """
+        data_dir = parameters.get("data_dir", None)
+        if data_dir is None or not os.path.isdir(data_dir):
+            raise Exception(f"Missing data directory: {data_dir}")
+        logger.info(f"Running {self.tool} on data_dir: {data_dir}")
+  
+        shell_command = f"dtcc/builder/bin/dtcc-builder --citymodel-only {data_dir}"
+ 
+        return shell_command
+ 
+    def parse_stdout(self, line:str):
+        """
+        Parse to the stdout line to extract 
+        1) progress percentage
+        2) log level
+        3) clean message
+        """
+        percent = 10
+        loglevel = "info"
+        message = line
+        return percent, loglevel, message
+    
+    def process_input(self, parameters:dict) -> None:
+        pass
+
+        
+   
+    def process_output(self, parameters:dict) -> str:
+        data_dir = parameters.get("data_dir", None)
+        if data_dir is None or not os.path.isdir(data_dir):
+            return json.dumps({"data_url": "ERROR"})
+        citymodel = os.path.join(data_dir, "CityModel.pb")
+        if not os.path.isfile(citymodel):
+            return json.dumps({"data_url": "ERROR"})
+        return json.dumps({"data_url": citymodel})
+
+class DtccGenerateSurfaceMesh(RunInShell):
+    def __init__(self, publish=True) -> None:
+        RunInShell.__init__(self,
+            module="dtcc-module-dtcc-builder",
+            tool="generate-surface-mesh",
+            publish=publish,
+            shell_command=""
+        )
+
+
+    def run_command(self, parameters:dict) -> str:
+        """
+        Pass in arguments based on the recived parameters if needed
+        Otherwise just return the original shell command
+        """
+        data_dir = parameters.get("data_dir", None)
+        if data_dir is None or not os.path.isdir(data_dir):
+            raise Exception(f"Missing data directory: {data_dir}")
+        logger.info(f"Running {self.tool} on data_dir: {data_dir}")
+  
+        shell_command = f"dtcc/builder/bin/dtcc-builder {data_dir}"
+ 
+        return shell_command
+ 
+    def parse_stdout(self, line:str):
+        """
+        Parse to the stdout line to extract 
+        1) progress percentage
+        2) log level
+        3) clean message
+        """
+        percent = 10
+        loglevel = "info"
+        message = line
+        return percent, loglevel, message
+    
+    def process_input(self, parameters:dict) -> None:
+        pass
+
+        
+   
+    def process_output(self, parameters:dict) -> str:
+        data_dir = parameters.get("data_dir", None)
+        if data_dir is None or not os.path.isdir(data_dir):
+            return json.dumps({"data_url": "ERROR"})
+        citysurface = os.path.join(data_dir, "CitySurface.pb")
+        if not os.path.isfile(citymodel):
+            return json.dumps({"data_url": "ERROR"})
+        return json.dumps({"data_url": citysurface})
+
 
 if __name__ == "__main__":
-    DtccBuilderCityModel(publish=True).listen()
+    logger.info("Starting dtcc-module-hello-world")
+    DtccCalcBuildingHeights(publish=True).listen()
+    DtccGenerateSurfaceMesh(publish=True).listen()
