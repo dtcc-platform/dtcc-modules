4b29209508d317df250105f810c172fce0012dbc
sidkas <sidkas@protonmail.ch>
2023-01-16 14:20:22 +0100

    init city solr with minio

--- dtcc-module-citysolar/wrappers/citysolar_irradiance.py
+++ dtcc-module-citysolar/wrappers/citysolar_irradiance.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 
-import subprocess
-import tempfile
-import os, sys, pathlib
-import json
+import os, sys, pathlib, json, time
 
 from core.run_in_shell import RunInShell
 from core.pub_sub_base import PubSubBase
@@ -40,13 +37,15 @@ class DtccCitySolarIrradiance(RunInShell,PubSubBase):
             file_name=file_name,
             bucket_name=bucket_name
         )
-        inputfile_path = os.path.join(self.local_data_dir, prefix, file_name)
+        inputfile_path = os.path.join(self.local_data_dir, file_name)
         latitude = parameters.get("latitude", None)
         longitude = parameters.get("longitude", None)
         date = parameters.get("date", None)
 
-        shell_command = f"python3 -m citysolar.RunSolar --inputfile {inputfile_path} --latitude {latitude} --longitude {longitude} --one_date {date} --exportpath {self.output_file_name}"
- 
+        ## Optional: --latitude {latitude} --longitude {longitude} --one_date {date}
+
+        shell_command = f"python3 -m citysolar.run_solar --display False --inputfile '{inputfile_path}' --exportpath '{self.output_file_name}' " 
+
         return shell_command
  
     def parse_stdout(self, line:str):
@@ -69,9 +68,15 @@ class DtccCitySolarIrradiance(RunInShell,PubSubBase):
         bucket_name = parameters.get("bucket_name", None)
         prefix = parameters.get("prefix", None)
         file_name = parameters.get("file_name", None)
-
-        if not os.path.isfile(self.output_file_name):
-            return self.create_output(status="Error: File not available!")
+        
+        wait_for_file = 5
+        for n in range(wait_for_file):
+            if not os.path.isfile(self.output_file_name):
+                if n == wait_for_file-1:
+                    return self.create_output(status="Error: File not available!")
+            else:
+                break
+            time.sleep(10)
         
         file_info = self.upload_file(file_path=self.output_file_name,prefix=prefix,bucket_name=bucket_name)
 
---
+++ dtcc-module-examples/README.md
@@ -0,0 +1,10 @@
+
+
+
+Request to start:
+{
+  "task_id": "556cdb98",
+  "module_name": "dtcc-module-examples",
+  "tool": "upload-download-minio",
+  "parameters": "{\"bucket_name\":\"dtcc\", \"prefix\":\"/HelsingborgHarbour2022\"}"
+}
